da3bd31464734ec54035a7b54ed35be2381b156c
Bartosz <gazdrinker2@gmail.com>
2024-04-12 14:07:14 +0200

    add dashboard calorie summary

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,13 @@
 //styles
 import './Dashboard.css';
 
-
+//components
+import Calories from '../../components/calories/Calories'
 
 export default function Dashboard() {
 	return (
 		<section className='dashboard wrapper'>
-      dashboard
+			<Calories />
 		</section>
 	);
 }
