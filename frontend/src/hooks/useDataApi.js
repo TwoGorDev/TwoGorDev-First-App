c49bafe3e1420519be8623e9c3a813af716d9a54
HoffmanSan <gortychmaciej97@gmail.com>
2024-04-17 19:52:56 +0200

    made by HoffmanSan

--- frontend/src/hooks/useDataApi.js
+++ frontend/src/hooks/useDataApi.js
@@ -2,18 +2,19 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default function useDataApi(endpoint, body) {
+const userToken = JSON.parse(localStorage.getItem('user-token')) || '';
+
+export default function useDataApi() {
   const [error, setError] = useState('');
   const [isPending, setIsPending] = useState(false);
   const [data, setData] = useState();
-  const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzksImlhdCI6MTcxMzE4NjkzMiwiZXhwIjoxNzE1Nzc4OTMyfQ._O4AqurNGBaMfLlO8-HYBKLtZq1YtxdQqgg2NR7rzsM';
 
-  const getData = async () => {
+  const getData = async (endpoint) => {
     setIsPending(true);
     setError('');
 
     await axios.get(
-      `http://localhost:4000/${endpoint}`,
+      `http://localhost:4000${endpoint}`,
       { headers: {'Authorization': `Bearer ${userToken}`} }
     )
     .then(res => setData(res.data))
@@ -30,9 +31,12 @@ export default function useDataApi(endpoint, body) {
     .finally(() => setIsPending(false))
   }
 
-  const postData = async () => {
+  const postData = async (endpoint, body) => {
+    setIsPending(true);
+    setError('');
+    
     await axios.post(
-      `http://localhost:4000/${endpoint}`,
+      `http://localhost:4000${endpoint}`,
       body,
       { headers: {'Authorization': `Bearer ${userToken}`} }
     )
--- frontend/src/utilities/getCurrentDate.js
+++
@@ -1,7 +0,0 @@
-const date = new Date();
-const year = date.getFullYear();
-const month = `0${date.getMonth() + 1}`.slice(-2);
-const day = `0${date.getDate()}`.slice(-2);
-const formattedDate = `${year}-${month}-${day}`
-
-export default formattedDate;
