b0cd6dc7be2cf0e95247cd1dc92a4f4be4b389d8
Bartosz <gazdrinker2@gmail.com>
2024-04-19 17:16:22 +0200

    add mobile nav

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,54 +1,57 @@
 import { Link, NavLink, useLocation } from 'react-router-dom';
-import { useState } from 'react'
+import { useState } from 'react';
 
 // styles
 import './Navbar.css';
 
-
 export default function Navbar() {
-  const location = useLocation();
-  const [navMobileActive, setNavMobileActive] = useState(false);
+	const location = useLocation();
+	const [navMobileActive, setNavMobileActive] = useState(true);
 
-  const navClass = ['/', '/register', '/login'].includes(location.pathname)? 'home-nav' : 'nav';
+	const navClass = ['/', '/register', '/login'].includes(location.pathname)
+		? 'home-nav'
+		: 'nav';
 
-  return (
-    <nav className={navClass}>
-      <div className="hamburger" onClick={() => setNavMobileActive(prevState => !prevState)}>
-        <div className="hamburger-line"></div>
-        <div className="hamburger-line"></div>
-        <div className="hamburger-line"></div>
-      </div>
-      <ul className='nav-list wrapper'>
-        <li className='logo'>
-          <Link className='logo-link' to='/'>
-            HealThyBody
-          </Link>
-        </li>
-        
-          <>
-            <li>
-              <NavLink className='nav-link' to='dashboard'>
-                Dashboard
-              </NavLink>
-            </li>
-            <li>
-              <NavLink className='nav-link' to='account'>
-                Account
-              </NavLink>
-            </li>
-            <li>
-              <NavLink className='nav-link' to='register'>
-                Register
-              </NavLink>
-            </li>
-            <li>
-              <NavLink className='nav-link' to='login'>
-                Login
-              </NavLink>
-            </li>
-          </>
-        
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className={`${navClass} ${navMobileActive ? 'mobile-active' : ''}`}>
+			<div className='nav-headings-container'>
+				<div className='logo'>
+					<Link className='logo-link' to='/'>
+						HealThyBody
+					</Link>
+				</div>
+
+				<div
+					className='hamburger'
+					onClick={() => setNavMobileActive((prevState) => !prevState)}>
+					<div className='hamburger-line'></div>
+					<div className='hamburger-line'></div>
+					<div className='hamburger-line'></div>
+				</div>
+			</div>
+
+			<ul className='nav-list wrapper'>
+				<li>
+					<NavLink onClick={() => setNavMobileActive(false)} className='nav-link' to='/dashboard'>
+						Dashboard
+					</NavLink>
+				</li>
+				<li>
+					<NavLink onClick={() => setNavMobileActive(false)} className='nav-link' to='/account'>
+						Account
+					</NavLink>
+				</li>
+				<li>
+					<NavLink onClick={() => setNavMobileActive(false)} className='nav-link' to='/register'>
+						Register
+					</NavLink>
+				</li>
+				<li>
+					<NavLink onClick={() => setNavMobileActive(false)} className='nav-link' to='/login'>
+						Login
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 }
