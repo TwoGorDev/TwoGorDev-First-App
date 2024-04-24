19fb87213cc3f7cee0732b1303806c249f548449
Bartosz <gazdrinker2@gmail.com>
2024-04-24 17:37:49 +0200

    rwd

--- frontend/src/components/productsTable/ProductsTable.jsx
+++ frontend/src/components/productsTable/ProductsTable.jsx
@@ -9,10 +9,16 @@ import { FaCirclePlus } from 'react-icons/fa6';
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function ProductsTable({ addProduct, setTotalProductCalories, setNewPortions, products }) {
-	const [isProductServingModalOpen, setIsProductServingModalOpen] = useState(false);
+export default function ProductsTable({
+	addProduct,
+	setTotalProductCalories,
+	setNewPortions,
+	products,
+}) {
+	const [isProductServingModalOpen, setIsProductServingModalOpen] =
+		useState(false);
 	const [selectedProduct, setSelectedProduct] = useState(null);
-	
+
 	// Check if this component is used on 'products' page
 	const { pathname } = useLocation();
 	const isProductsPage = pathname === '/dashboard/products';
@@ -25,7 +31,6 @@ export default function ProductsTable({ addProduct, setTotalProductCalories, set
 
 	return (
 		<div className='table-container'>
-			
 			<table
 				className='products-table'
 				style={isProductsPage ? { height: '60vh' } : {}}>
@@ -41,38 +46,37 @@ export default function ProductsTable({ addProduct, setTotalProductCalories, set
 					</tr>
 				</thead>
 				<tbody>
-					{products && products.map((item) => (
-						<tr key={item.id} className='products-table-data product'>
-							<td className='spacer'></td>
-							<td>{item.name}</td>
-							<td>{`${item.calories} `}</td>
-							<td>{`${item.carbohydrates}g`}</td>
-							<td>{`${item.proteins}g`}</td>
-							<td>{`${item.fats}g`}</td>
-							{/* Do not display the add button on 'products' page*/}
-							<td>
-								{!isProductsPage && (
-									<FaCirclePlus
-										className='add-meal-button'
-										onClick={() => handleIconClick(item)}
-									/>
-								)}
-
-								
-							</td>
-						</tr>
-					))}
+					{products &&
+						products.map((item) => (
+							<tr key={item.id} className='products-table-data product'>
+								<td className='spacer'></td>
+								<td>{item.name}</td>
+								<td>{`${item.calories} `}</td>
+								<td>{`${item.carbohydrates}g`}</td>
+								<td>{`${item.proteins}g`}</td>
+								<td>{`${item.fats}g`}</td>
+								{/* Do not display the add button on 'products' page*/}
+								<td>
+									{!isProductsPage && (
+										<FaCirclePlus
+											className='add-meal-button'
+											onClick={() => handleIconClick(item)}
+										/>
+									)}
+								</td>
+							</tr>
+						))}
 				</tbody>
 			</table>
 			{isProductServingModalOpen && (
-									<ProductServingModal
-										setOpenServingModal={setIsProductServingModalOpen}
-										product={selectedProduct}
-										addProduct={addProduct}
-										setTotalProductCalories={setTotalProductCalories}
-										setNewPortions={setNewPortions}
-									/>
-								)}
+				<ProductServingModal
+					setOpenServingModal={setIsProductServingModalOpen}
+					product={selectedProduct}
+					addProduct={addProduct}
+					setTotalProductCalories={setTotalProductCalories}
+					setNewPortions={setNewPortions}
+				/>
+			)}
 		</div>
 	);
 }
